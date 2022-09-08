576b33e149221674922eba9681ff06b137170067
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-09-08 13:41:05 +0530

    Day05

---
+++ Typescript/Day05/demo1.js
@@ -0,0 +1,29 @@
+//loop Statements
+// for loop,while,do-while,foreach
+// for(var i=0;i<5;i++){
+//   console.log("Value of i "+i);
+// }
+// console.log("After for loop i is "+i);
+// var keyward has global scope 
+// let keyward => It is also used for variable creation
+// let has scope => within nearest block ({}/file block)
+// for(let i=0;i<5;i++){
+//   console.log("Using let Value of i "+i);
+// }
+// console.log("After the loop access i variable "+i);
+// const => It is also used for variable creation
+// it can't change the value of that variable through out the program
+// it has global scope as well as local scope
+var pi = 3.14;
+// console.log("Constant value is "+pi);
+// while
+var count = 5;
+// while(count!=0){
+//   console.log("Count is "+count);
+//   count--;
+// }
+//do-while
+do {
+    console.log("Count is " + count);
+    count--;
+} while (count != 0);
---
+++ Typescript/Day05/demo2.js
@@ -0,0 +1,27 @@
+//functions
+// 1 Function without paramter & without return type
+// 2 Function with paramter & without return type
+// 3 Function without paramter & with return type
+// 4 Function with paramter & with return type
+// 1 Function without paramter & without return type
+function add1() {
+    console.log("First type of function is called ");
+}
+// add1(); //calling function
+// 2 Function with paramter & without return type
+function add2(a, b) {
+    console.log("Addition is " + (a + b));
+}
+// add2(3,4);
+// 3 Function without paramter & with return type
+function add3() {
+    return (4 + 4);
+}
+// let res=add3();
+// console.log(`Result is ${res}`);
+// 4 Function with paramter & with return type
+function add4(a, b) {
+    return (a + b);
+}
+// console.log(` Additio  is ${add4(10,8)} `);
+console.log(" Additio  is  " + add4(5, 4));
