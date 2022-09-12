9c89d415be5997187181b39ba5435e768c170558
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-09-12 13:28:17 +0530

    Day06

---
+++ Typescript/Day06/demo1.ts
@@ -0,0 +1,95 @@
+//Anonymous Function
+
+// function does not have any name
+
+  let temp1=function (){
+    console.log("Anonymous Function is called of first Type function")
+  }
+
+  // temp1();
+
+    let temp2=function (a:number,b:number):number{
+      return (a+b);
+    }
+
+      // let res1=temp2(4,6);
+      // console.log("Result is "+res1);
+
+//fat arrow function/Arrow function
+
+    let temp3=()=>{
+        console.log("Fat Arrow Function is called ")
+     }
+
+      //  temp3();
+    
+      let temp4=(a:number,b:number):number=>{
+        return (a+b)
+      }
+
+        
+        // console.log("Result is "+temp4(7,3));
+
+    
+// Optional parameter Function
+
+      function add1(a:number,b?:number){
+
+          console.log("value of a is "+a); // 5
+          console.log("value of b is "+b); // undefined
+          console.log("Addition of a+b is "+(a+b!)) // NAN => Not a number
+
+      }
+
+      // add1(5);
+
+      function add2(a?:number,b?:number){
+
+        console.log("value of a is "+a); // 5
+        console.log("value of b is "+b); // undefined
+        console.log("Addition of a+b is "+(a!+b!)) // NAN => Not a number
+
+    }
+
+    // add2();
+    // add2(8,4)
+
+    // default parameter functions 
+
+    function add3(a:number,b:number=8){
+      console.log("value of a is "+a); // 10
+      console.log("value of b is "+b); // 9
+      console.log("Addition of a+b is "+(a!+b!)) // 19
+
+    }
+
+    // add3(10);
+
+    // add3(10,9);
+
+    function add4(a:number=5,b:number){
+      console.log("value of a is "+a); // 10
+      console.log("value of b is "+b); // 9
+      console.log("Addition of a+b is "+(a!+b!)) // 19
+
+    }
+
+    // add4(7,11)
+
+    function add5(a:number=5,b?:number){
+      console.log("value of a is "+a); // 10
+      console.log("value of b is "+b); // 9
+      console.log("Addition of a+b is "+(a!+b!)) // 19
+
+    }
+
+    // add5();
+    // add5(10);
+    add5(10,45);
+
+
+
+  
+
+
+
