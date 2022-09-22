fed1fdf099534d41678f3f6e0492e0e7719b0abe
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-09-22 13:49:27 +0530

    Day10

---
+++ Day10/MainInheritance.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var circle_1 = require("./circle");
+var ractangle_1 = require("./ractangle");
+var circleobj = new circle_1.Circle(2);
+circleobj.Myarea();
+circleobj.display();
+var rectobj = new ractangle_1.Rectangle(2, 3);
+rectobj.Myarea();
+rectobj.display();
---
+++ Day10/MainInheritance.ts
@@ -0,0 +1,12 @@
+
+import { Circle } from "./circle";
+import { Rectangle } from "./ractangle";
+
+let circleobj=new Circle(2);
+circleobj.Myarea();
+circleobj.display();
+
+
+let rectobj=new Rectangle(2,3);
+rectobj.Myarea();
+rectobj.display();
