88307dd72c32f3bce88ab6fe0ec3d26dbf22ee81
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-09-29 13:56:23 +0530

    Day04

---
+++ Angular/Day04/eventbinding2/eventbinding2.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventbinding2',
+  templateUrl: './eventbinding2.component.html',
+  styleUrls: ['./eventbinding2.component.css']
+})
+export class Eventbinding2Component implements OnInit {
+
+  result:number=0;
+  result1:number=0;
+  name:string='';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddition(num1:any,num2:any){
+
+    let n1=+num1;
+    let n2=+num2;
+
+      // this.result=num1+num2;
+      this.result=n1+n2;
+  }
+
+  onChange(){
+    console.log("Change event occur...")
+  }
+
+  onKeyup(){
+    console.log("Key Up event Occur....")
+  }
+
+  onKeydown(){
+    console.log("key down event occur...")
+  }
+
+}
